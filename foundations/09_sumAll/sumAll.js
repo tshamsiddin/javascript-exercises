9a0244fc34a0b46e4af58f970e158f7e704ee45b
tshamsiddin <84608684+tshamsiddin@users.noreply.github.com>
2026-07-05 15:01:12 +0500

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const isPosInt=(num)=>Number.isInteger(num) && num>0;
 
+const sumAll = function(a, b) {
+    if (!isPosInt(a) || !isPosInt(b)){
+        return "ERROR";
+    }
+
+    let total=0;
+
+    if (a>=b){
+        for (let i=a;i>=b;i--){
+            total+=i;
+        }
+    }
+
+    if (a<=b){
+        for (let i=a;i<=b;i++){
+            total+=i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
