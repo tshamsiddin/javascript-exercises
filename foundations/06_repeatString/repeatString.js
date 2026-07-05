800d46f95bf437a7424a48cd12d81f7bcdae9b8c
tshamsiddin <84608684+tshamsiddin@users.noreply.github.com>
2026-07-05 14:17:26 +0500

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedString="";
+    if (num<0){
+        return "ERROR";
+    }
+    for (let i=0;i<num;i++){
+        repeatedString+=str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
