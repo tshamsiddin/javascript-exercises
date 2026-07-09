2dd9bf08f9863a25a68f51637522033d307025b2
tshamsiddin <84608684+tshamsiddin@users.noreply.github.com>
2026-07-09 10:20:49 +0500

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) return "OOPS";
+    if (num < 1) return 0;
+    a=1;
+    b=1;
+    for (i=3;i<=num;i++){
+        let nextNumber=a+b;
+        a=b;
+        b=nextNumber;                               
+    }
+    return b;
 };
 
 // Do not edit below this line
