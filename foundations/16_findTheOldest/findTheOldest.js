b82dc8797fe61bfbef238083570b12efd7c11160
tshamsiddin <84608684+tshamsiddin@users.noreply.github.com>
2026-07-09 10:39:07 +0500

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear=new Date().getFullYear();
+    const ordered=arr.sort((a, b) => {
+        const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        return ageB-ageA;
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
